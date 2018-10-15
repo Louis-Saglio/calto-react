8fb0c33cd9848552bc04ac455615a9975bd27d7d
Louis-Saglio <louis.saglio@sfr.fr>
2018-10-15 15:42:36 +0200

    Remove eval usage

--- src/App.js
+++ src/App.js
@@ -1,28 +1,80 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+class Button extends Component {
+    render() {
+        return <button onClick={this.props.onClick}>{this.props.text}</button>
+    }
 }
 
-export default App;
+
+class Calculator extends Component {
+    constructor() {
+        super();
+        this.state = {
+            expression: '0',
+            numbers: ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.'],
+            operations: {
+                '+': (x, y) => x + y,
+                '-': (x, y) => x - y,
+                '/': (x, y) => x / y,
+                '*': (x, y) => x * y
+            },
+            nextOperation: null,
+            _isSecond: false,
+            firstExp: null,
+            secondExp: null
+        }
+    }
+
+    click(text) {
+        this.setState((prevState) => {
+            if (this.state._isSecond) {
+                return {secondExp: (prevState.secondExp == null ? text : prevState.secondExp + text)}
+            } else {
+                return {firstExp: (prevState.firstExp == null ? text : prevState.firstExp + text)}
+            }
+        })
+    }
+
+    setOperation(operation) {
+        this.setState({
+            nextOperation: operation,
+            _isSecond: true
+        })
+    }
+
+    clear() {
+        this.setState({
+            nextOperation: null,
+            _isSecond: true,
+            firstExp: null,
+            secondExp: null
+        })
+    }
+
+    run() {
+        if (this.state.firstExp !== null && this.state.secondExp !== null) {
+            this.setState({
+                nextOperation: null,
+                _isSecond: null,
+                firstExp: this.state.operations[this.state.nextOperation](parseFloat(this.state.firstExp), parseFloat(this.state.secondExp)),
+                secondExp: null
+            })
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <p>{this.state.firstExp == null ? '0' : this.state.firstExp} {this.state.nextOperation} {this.state.secondExp}</p>
+                {this.state.numbers.map(number => <Button onClick={(e) => this.click(e.target.innerHTML)} text={number}/>)}
+                {Object.keys(this.state.operations).map(operation => <Button onClick={(e) => this.setOperation(e.target.innerHTML)} text={operation}/>)}
+                <Button onClick={() => this.clear()} text='AC'/>
+                <Button onClick={() => this.run()} text='='/>
+            </div>
+        )
+    }
+}
+
+export default Calculator;
